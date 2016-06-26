79fba316c8726bc45dd488df6cac0cb8876f4ca8
Tomonori <tomonori0204.takahashi@gmail.com>
2016-06-26 18:32:10 +0900

    index画面作成

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,8 +1,11 @@
 class RestaurantsController < ApplicationController
   def index
+    @restaurants = Restaurant.all
+    @new_restaurants = Restaurant.all
   end
 
   def show
+    @restaurant = Restaurant.find(params[:id])
   end
 
   def new
@@ -16,12 +19,17 @@ class RestaurantsController < ApplicationController
   end
 
   def edit
+    @restaurant = Restaurant.find(params[:id])
   end
 
   def update
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.update(post_params)
+    redirect_to restaurant_path(@restaurant)
   end
 
   def destroy
+    @restaurant = Restaurant.find(params[:id])
   end
 
   private
